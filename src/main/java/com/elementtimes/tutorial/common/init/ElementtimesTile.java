4dc86fb4457c467076e479fc62706c585d496d77
ksgfk <37179748+ksgfk@users.noreply.github.com>
2019-05-06 14:11:24 +0800

    忘记注册TileEntity了

--- src/main/java/com/elementtimes/tutorial/common/init/ElementtimesTile.java
+++ src/main/java/com/elementtimes/tutorial/common/init/ElementtimesTile.java
@@ -2,6 +2,7 @@ package com.elementtimes.tutorial.common.init;
 
 import com.elementtimes.tutorial.Elementtimes;
 import com.elementtimes.tutorial.common.tileentity.TileElementGenerater;
+import com.elementtimes.tutorial.common.tileentity.TilePulverizer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
@@ -13,5 +14,6 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 public class ElementtimesTile {
     public static void init() {
         GameRegistry.registerTileEntity(TileElementGenerater.class, new ResourceLocation(Elementtimes.MODID + ":" + "element_generater"));
+        GameRegistry.registerTileEntity(TilePulverizer.class, new ResourceLocation(Elementtimes.MODID + ":" + "pulverizer"));
     }
 }
