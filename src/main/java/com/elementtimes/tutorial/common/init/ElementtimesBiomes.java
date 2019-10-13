9223070a8290765070f7287890ac7eaf8330dfe4
luqin2007 <lq2007lq@hotmail.com>
2019-10-13 14:11:03 +0800

    取消 Biome 注册

--- src/main/java/com/elementtimes/tutorial/common/init/ElementtimesBiomes.java
+++ src/main/java/com/elementtimes/tutorial/common/init/ElementtimesBiomes.java
@@ -1,9 +1,6 @@
 package com.elementtimes.tutorial.common.init;
 
-import com.elementtimes.tutorial.common.biome.ExampleBiome;
-import net.minecraft.world.biome.Biome;
-
 public class ElementtimesBiomes {
 
-    public static Biome sea = new ExampleBiome();
+//    public static Biome sea = new ExampleBiome();
 }
