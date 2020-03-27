54653b99a7c30bfc2d76566bf0fe1a98d07ac244
luqin2007 <lq2007lq@hotmail.com>
2020-03-27 10:02:04 +0800

    元素镐附魔切换

--- src/main/java/com/elementtimes/tutorial/common/item/Elementpickaxe.java
+++ src/main/java/com/elementtimes/tutorial/common/item/Elementpickaxe.java
@@ -2,12 +2,21 @@ package com.elementtimes.tutorial.common.item;
 
 import com.elementtimes.elementcore.api.common.ECUtils;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Enchantments;
 import net.minecraft.item.ItemPickaxe;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.NonNullList;
+import net.minecraft.world.World;
 import net.minecraftforge.common.util.EnumHelper;
 
 import javax.annotation.Nonnull;
+import java.util.Map;
 
 public class Elementpickaxe  extends ItemPickaxe {
 
@@ -20,5 +29,24 @@ public class Elementpickaxe  extends ItemPickaxe {
 	public void getSubItems(@Nonnull CreativeTabs tab, @Nonnull NonNullList<ItemStack> items) {
 		super.getSubItems(tab, items);
 		items.stream().filter(itemStack -> itemStack.getItem() == this).forEach(ECUtils.item::addAllEnchantments);
+	}
+
+	@Override
+	public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer player, EnumHand hand) {
+		if (!worldIn.isRemote) {
+			ItemStack stack = player.getHeldItem(hand);
+			Map<Enchantment, Integer> enchantments = EnchantmentHelper.getEnchantments(stack);
+			if (enchantments.containsKey(Enchantments.FORTUNE)) {
+				enchantments.remove(Enchantments.FORTUNE);
+				enchantments.put(Enchantments.SILK_TOUCH, Enchantments.SILK_TOUCH.getMaxLevel());
+				EnchantmentHelper.setEnchantments(enchantments, stack);
+			} else if (enchantments.containsKey(Enchantments.SILK_TOUCH)) {
+				enchantments.remove(Enchantments.SILK_TOUCH);
+				enchantments.put(Enchantments.FORTUNE, Enchantments.FORTUNE.getMaxLevel());
+				EnchantmentHelper.setEnchantments(enchantments, stack);
+			}
+			return new ActionResult<>(EnumActionResult.PASS, stack);
+		}
+		return super.onItemRightClick(worldIn, player, hand);
 	}
 }
